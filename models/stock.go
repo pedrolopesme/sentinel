508fc93c7a3cadc39ebd746cbfbf354b874126fc
Pedro Mendes <pedrolopesme@gmail.com>
2019-02-12 23:36:32 -0200

    minor: add Stock models

---
+++ models/stock.go
@@ -0,0 +1,18 @@
+package models
+
+// Money extracts currency to its own type
+type Money float64
+
+// StockPrice stores the price variation within a range of time
+type StockPrice struct {
+	Open  Money
+	Close Money
+	High  Money
+	Low   Money
+}
+
+// StockTier joins price variation with other important information
+type StockTier struct {
+	Volume int64
+	*StockPrice
+}
